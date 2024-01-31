b619c0abed16c668c0714d6d9bbb686a93892eed
EAG <mataramosmiguel@gmail.com>
2024-01-31 16:41:36 +0100

    login creado

--- app/src/main/java/com/example/practicafinal/Registrarse.kt
+++ app/src/main/java/com/example/practicafinal/Registrarse.kt
@@ -3,67 +3,65 @@ package com.example.practicafinal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.practicafinal.databinding.ActivityMainBinding
+import android.widget.Toast
+import com.example.practicafinal.databinding.ActivityRegistrarseBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var bind: ActivityMainBinding
+class Registrarse : AppCompatActivity() {
     private lateinit var autentication: FirebaseAuth
     private var email: String? = null
     private var password: String? = null
+    private lateinit var bind: ActivityRegistrarseBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bind = ActivityMainBinding.inflate(layoutInflater)
+        bind = ActivityRegistrarseBinding.inflate(layoutInflater)
         setContentView(bind.root)
 
         autentication = FirebaseAuth.getInstance()
 
-        bind.textViewNoCuenta.setOnClickListener {
-            var intent = Intent(this, Registrarse::class.java)
-            startActivity(intent)
-        }
-
-        bind.buttonLogin.setOnClickListener {
+        bind.buttonRegistrarse.setOnClickListener {
             if (cogerEmail() != null && cogerPassword() != null) {
-                autentication.signInWithEmailAndPassword(email!!, password!!)
+                autentication.createUserWithEmailAndPassword(email!!, password!!)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
-                            var intent = Intent(this, Ver::class.java)
+                            Toast.makeText(this, "Registrado correctamente", Toast.LENGTH_SHORT)
+                                .show()
+                            var intent = Intent(this, MainActivity::class.java)
                             startActivity(intent)
                         } else {
-                            bind.textInputLayoutEmail.error = "Email o contraseña incorrectos"
-                            bind.textInputLayoutPass.error = "Email o contraseña incorrectos"
+                            Toast.makeText(this, "Error al registrarse", Toast.LENGTH_SHORT).show()
                         }
                     }
             }
         }
-
     }
 
+
     private fun cogerEmail(): String? {
-        return if (bind.mail.text.toString().isNullOrBlank()){
+        return if (bind.mail.text.toString().isNullOrBlank()) {
             bind.textInputLayoutEmail.error = "Email esta vacio"
             null
-        }else{
+        } else if (!bind.mail.text.toString().contains("@")) {
+            bind.textInputLayoutEmail.error = "Email no valido"
+            null
+        } else {
             bind.textInputLayoutEmail.error = null
             email = bind.mail.text.toString()
             email
         }
-
     }
+
     private fun cogerPassword(): String? {
         return if (bind.pass.text.toString().isNullOrBlank()) {
             bind.textInputLayoutPass.error = "Contraseña esta vacia"
             null
-        }else{
+        } else {
             bind.textInputLayoutPass.error = null
             password = bind.pass.text.toString()
             password
         }
 
     }
-
-
 }
